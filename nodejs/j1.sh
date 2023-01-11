5e36b8842a21ea61757f535099789d7947818ad7
kangbock <110449043+kangbock@users.noreply.github.com>
2023-01-11 13:08:28 +0900

    step 1

---
+++ nodejs/j1.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+sudo sed -i '% s/20.249.92.238/20.214.219.101/g' project_v2/nodejs/main3.js
+sudo sed -i '% s/20.249.92.238/20.214.219.101/g' project_v2/nodejs/main.html
+sudo sed -i '% s/20.249.92.238/20.214.219.101/g' project_v2/nodejs/board.html
+sudo sed -i '% s/20.249.92.238/20.214.219.101/g' project_v2/nodejs/write.html
+sudo sed -i '% s/20.249.92.238/20.214.219.101/g' project_v2/nodejs/organization.html
