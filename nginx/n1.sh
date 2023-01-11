441d52b4f10d66cd5f89bbe279a126023cf18ed8
kangbock <110449043+kangbock@users.noreply.github.com>
2023-01-11 13:07:16 +0900

    step3

--- nginx/n1.sh
+++ nginx/n1.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-sudo sed -i '% s/www.kb97.xyz:3000/node.kb97.xyz:3000/g' project/nginx/default.conf
-sudo sed -i '% s/20.249.92.238/20.214.219.101/g' project/nginx/login.html
-sudo sed -i '% s/20.249.92.238/20.214.219.101/g' project/nginx/logout.html
-sudo sed -i '% s/20.249.92.238/20.214.219.101/g' project/nginx/register.html
-sudo sed -i '% s/20.249.92.238/20.214.219.101/g' project/nodejs/board.html
-sudo sed -i '% s/20.249.92.238/20.214.219.101/g' project/nodejs/main.html
-sudo sed -i '% s/20.249.92.238/20.214.219.101/g' project/nodejs/main3.js
-sudo sed -i '% s/20.249.92.238/20.214.219.101/g' project/nodejs/organization.html
-sudo sed -i '% s/20.249.92.238/20.214.219.101/g' project/nodejs/write.html
+sudo sed -i '% s/www.kb97.xyz:3000/node.kb97.xyz:3000/g' project_v2/nginx/default.conf
+sudo sed -i '% s/20.249.92.238/20.214.219.101/g' project_v2/nginx/login.html
+sudo sed -i '% s/20.249.92.238/20.214.219.101/g' project_v2/nginx/logout.html
+sudo sed -i '% s/20.249.92.238/20.214.219.101/g' project_v2/nginx/register.html
+sudo sed -i '% s/20.249.92.238/20.214.219.101/g' project_v2/nodejs/board.html
+sudo sed -i '% s/20.249.92.238/20.214.219.101/g' project_v2/nodejs/main.html
+sudo sed -i '% s/20.249.92.238/20.214.219.101/g' project_v2/nodejs/main3.js
+sudo sed -i '% s/20.249.92.238/20.214.219.101/g' project_v2/nodejs/organization.html
+sudo sed -i '% s/20.249.92.238/20.214.219.101/g' project_v2/nodejs/write.html
