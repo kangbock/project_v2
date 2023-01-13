72ba9599b3a1ef01c3bb31b90d525af76014a267
kangbock <110449043+kangbock@users.noreply.github.com>
2023-01-13 10:49:31 +0900

    step 4

--- nodejs/j1.sh
+++ nodejs/j1.sh
@@ -5,3 +5,5 @@ sudo sed -i ':% s/20.249.92.238/20.214.219.101/g' project_v2/nodejs/main.html
 sudo sed -i ':% s/20.249.92.238/20.214.219.101/g' project_v2/nodejs/board.html
 sudo sed -i ':% s/20.249.92.238/20.214.219.101/g' project_v2/nodejs/write.html
 sudo sed -i ':% s/20.249.92.238/20.214.219.101/g' project_v2/nodejs/organization.html
+sudo docker build -t kangbock/nodejs:0 project_v2/nodejs/.
+sudo docker push kangbock/nodejs:0
