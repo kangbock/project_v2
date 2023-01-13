bcc3112e7d9d53345332e330024fbcdfebb23951
kangbock <110449043+kangbock@users.noreply.github.com>
2023-01-13 11:11:50 +0900

    step 4

--- nginx/n1.sh
+++ nginx/n1.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-sudo sed -i ':% s/www.kb97.xyz:3000/node.kb97.xyz:3000/g' project_v2/nginx/default.conf
+sudo sed -i ':% s/www.kb97.xyz:3000/node.kb97.xyz/g' project_v2/nginx/default.conf
 sudo sed -i ':% s/20.249.92.238/20.214.219.101/g' project_v2/nginx/login.html
 sudo sed -i ':% s/20.249.92.238/20.214.219.101/g' project_v2/nginx/logout.html
 sudo sed -i ':% s/20.249.92.238/20.214.219.101/g' project_v2/nginx/register.html
