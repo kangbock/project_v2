ace561fdd2b8616dfde192cf61fea818e0b4efc4
kangbock <110449043+kangbock@users.noreply.github.com>
2023-01-13 10:38:09 +0900

    step 3

--- nginx/mysql.sh
+++ nginx/mysql.sh
@@ -2,8 +2,9 @@
 # mysql
 sudo apt-get install -y mysql-client-core-5.7
 sudo export LANG="ko_KR.UTF-8"
-mysql -h prod-mysql-fs.mysql.database.azure.com -u admin1 -pIt12345! --ssl << FOE
-  use project
+mysql -h prod-mysql-fs.mysql.database.azure.com -u admin1 -pIt12345! << FOE
+  create database project;
+  use project;
   create table member (
 	  id int AUTO_INCREMENT NOT NULL primary key,
 	  email varchar(30) NOT NULL,
@@ -22,8 +23,8 @@ mysql -h prod-mysql-fs.mysql.database.azure.com -u admin1 -pIt12345! --ssl << FO
   	FOREIGN KEY (member_id) REFERENCES member(id)
   	);
 
-  grant all privileges on project.* to 'root'@'%' IDENTIFIED BY 'It12345!';
-  grant all privileges on project.* to 'root'@'mydb.czgwhg0kozpu.ap-northeast-2.rds.amazonaws.com' IDENTIFIED BY 'It12345!';
+//  grant all privileges on project.* to 'root'@'%' IDENTIFIED BY 'It12345!';
+//  grant all privileges on project.* to 'root'@'mydb.czgwhg0kozpu.ap-northeast-2.rds.amazonaws.com' IDENTIFIED BY 'It12345!';
   flush privileges;
 
   SHOW GRANTS FOR CURRENT_USER;
