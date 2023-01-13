bf3c9d5972b620fe34af298523b28e620e53e75d
kangbock <110449043+kangbock@users.noreply.github.com>
2023-01-13 17:10:16 +0900

    Delete j1.sh

--- nodejs/j1.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-sudo sed -i ':% s/20.249.92.238/20.214.161.190/g' project_v2/nodejs/main3.js
-sudo sed -i ':% s/20.249.92.238/20.214.161.190/g' project_v2/nodejs/main.html
-sudo sed -i ':% s/20.249.92.238/20.214.161.190/g' project_v2/nodejs/board.html
-sudo sed -i ':% s/20.249.92.238/20.214.161.190/g' project_v2/nodejs/write.html
-sudo sed -i ':% s/20.249.92.238/20.214.161.190/g' project_v2/nodejs/organization.html
